2224712329e5d5a19e0eeaec3e5439b1f2535e42
Sergey Gorokh <luxawes@gmail.com>
2021-07-05 14:49:54 +0200

    first commit

---
+++ src/main/java/com/sgorokh/TimeSaver/TimeSaverApplication.java
@@ -0,0 +1,19 @@
+package com.sgorokh.TimeSaver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+@SpringBootApplication
+public class TimeSaverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TimeSaverApplication.class, args);
+	}
+
+	@Bean
+	public RestTemplate getRestTemplate() {
+		return new RestTemplate();
+	}
+}
